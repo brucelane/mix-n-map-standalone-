56b5b23a8371de0ad8912a152021dba9c031e67b
Bruce LANE <bruce@batchass.fr>
2015-01-03 19:11:25 +0100

    UI

--- src/MixnMap.cpp
+++ src/MixnMap.cpp
@@ -96,120 +96,253 @@ void MixnMap::draw()
 	mSpout->draw();
 	mTextures->draw();
 	mWarpings->draw();
-	// Show the user what it is receiving
-	gl::enableAlphaBlending();
-	char txtReceiver[256];
-	sprintf_s(txtReceiver, "Received:", mParameterBag->OSCMsg);
-	gl::drawString(txtReceiver, vec2(toPixels(20), toPixels(40)), Color(1, 1, 1), Font("Verdana", toPixels(24)));
-	sprintf_s(txtReceiver, "fps : %2.2d", (int)getAverageFps());
-	gl::drawString(txtReceiver, vec2(getWindowWidth() - toPixels(100), toPixels(40)), Color(1, 1, 1), Font("Verdana", toPixels(24)));
-
-	ImGui::NewFrame();
-
-	// start a new window
-	ImGui::Begin("MixnMap", NULL, ImVec2(500, 600));
+	if (mParameterBag->mShowUI)
 	{
-		// our theme variables
-		static float WindowPadding[2] = { 25, 10 };
-		static float WindowMinSize[2] = { 160, 80 };
-		static float FramePadding[2] = { 4, 4 };
-		static float ItemSpacing[2] = { 10, 5 };
-		static float ItemInnerSpacing[2] = { 5, 5 };
+		gl::enableAlphaBlending();
 
-		static float WindowFillAlphaDefault = 0.7;
-		static float WindowRounding = 4;
-		static float TreeNodeSpacing = 22;
-		static float ColumnsMinSpacing = 50;
-		static float ScrollBarWidth = 12;
-
-		if (ImGui::CollapsingHeader("Parameters", "1", true, true))
+		// UI
+		ImGui::NewFrame();
+		ImGui::SetNewWindowDefaultPos(ImVec2(0, 0));
+		// start a new window
+		ImGui::Begin("MixnMap", NULL, ImVec2(500, 600));
 		{
-			ImGui::InputInt("OSC receiver port", &mParameterBag->mOSCReceiverPort);
-			// foreground color
-			static float color[4] = { mParameterBag->controlValues[1], mParameterBag->controlValues[2], mParameterBag->controlValues[3], mParameterBag->controlValues[4] };
-			ImGui::ColorEdit4("color", color);
-			mParameterBag->controlValues[1] = color[0];
-			mParameterBag->controlValues[2] = color[1];
-			mParameterBag->controlValues[3] = color[2];
-			mParameterBag->controlValues[4] = color[3];
-			ImGui::TextColored(ImVec4(mParameterBag->controlValues[1], 0.0f, 0.0f, 1.0f), "Red"); ImGui::SameLine();
-			ImGui::TextColored(ImVec4(0.0f, mParameterBag->controlValues[2], 0.0f, 1.0f), "Green"); ImGui::SameLine();
-			ImGui::TextColored(ImVec4(0.0f, 0.0f, mParameterBag->controlValues[3], 1.0f), "Blue"); ImGui::SameLine();
-			ImGui::TextColored(ImVec4(mParameterBag->controlValues[1], mParameterBag->controlValues[2], mParameterBag->controlValues[3], mParameterBag->controlValues[4]), "Alpha");
+			ImVec4 color0 = ImVec4(0.431373, 0.160784, 0.372549, 1);
+			ImVec4 color1 = ImVec4(0.2, 0.0392157, 0.0392157, 1);
+			ImVec4 color2 = ImVec4(0.317647, 0.184314, 0.2, 1);
+			ImVec4 color3 = ImVec4(1, 0.647059, 1, 1);
+			ImVec4 color4 = ImVec4(0.741176, 0.0941176, 1, 1);
+			ImGui::setThemeColor(color0, color1, color2, color3, color4);
+			// our theme variables
+			static float WindowPadding[2] = { 4, 2 };
+			static float WindowMinSize[2] = { 160, 80 };
+			static float FramePadding[2] = { 4, 4 };
+			static float ItemSpacing[2] = { 10, 5 };
+			static float ItemInnerSpacing[2] = { 5, 5 };
 
-			// background color
-			static float backcolor[4] = { mParameterBag->controlValues[5], mParameterBag->controlValues[6], mParameterBag->controlValues[7], mParameterBag->controlValues[8] };
-			ImGui::ColorEdit4("back color", backcolor);
-			mParameterBag->controlValues[5] = backcolor[0];
-			mParameterBag->controlValues[6] = backcolor[1];
-			mParameterBag->controlValues[7] = backcolor[2];
-			mParameterBag->controlValues[8] = backcolor[3];
-			ImGui::TextColored(ImVec4(mParameterBag->controlValues[5], 0.0f, 0.0f, 1.0f), "Red"); ImGui::SameLine();
-			ImGui::TextColored(ImVec4(0.0f, mParameterBag->controlValues[6], 0.0f, 1.0f), "Green"); ImGui::SameLine();
-			ImGui::TextColored(ImVec4(0.0f, 0.0f, mParameterBag->controlValues[7], 1.0f), "Blue"); ImGui::SameLine();
-			ImGui::TextColored(ImVec4(mParameterBag->controlValues[5], mParameterBag->controlValues[6], mParameterBag->controlValues[7], mParameterBag->controlValues[8]), "Alpha");
+			static float WindowFillAlphaDefault = 0.35;
+			static float WindowRounding = 11;
+			static float TreeNodeSpacing = 22;
+			static float ColumnsMinSpacing = 50;
+			static float ScrollBarWidth = 12;
 
-			if (ImGui::Button("Save")) { mParameterBag->save(); }
-
-		}
-		if (ImGui::CollapsingHeader("Log", "2", true, true))
-		{
-			static bool pause;
-			static ImVector<float> values; if (values.empty()) { values.resize(100); memset(&values.front(), 0, values.size()*sizeof(float)); }
-			static int values_offset = 0;
-			if (!pause)
+			if (ImGui::CollapsingHeader("Parameters", "1", true, true))
 			{
-				static float refresh_time = -1.0f;
-				if (ImGui::GetTime() > refresh_time + 1.0f / 6.0f)
+				ImGui::InputInt("OSC receiver port", &mParameterBag->mOSCReceiverPort);
+				// foreground color
+				static float color[4] = { mParameterBag->controlValues[1], mParameterBag->controlValues[2], mParameterBag->controlValues[3], mParameterBag->controlValues[4] };
+				ImGui::ColorEdit4("color", color);
+				mParameterBag->controlValues[1] = color[0];
+				mParameterBag->controlValues[2] = color[1];
+				mParameterBag->controlValues[3] = color[2];
+				mParameterBag->controlValues[4] = color[3];
+				ImGui::TextColored(ImVec4(mParameterBag->controlValues[1], 0.0f, 0.0f, 1.0f), "Red"); ImGui::SameLine();
+				ImGui::TextColored(ImVec4(0.0f, mParameterBag->controlValues[2], 0.0f, 1.0f), "Green"); ImGui::SameLine();
+				ImGui::TextColored(ImVec4(0.0f, 0.0f, mParameterBag->controlValues[3], 1.0f), "Blue"); ImGui::SameLine();
+				ImGui::TextColored(ImVec4(mParameterBag->controlValues[1], mParameterBag->controlValues[2], mParameterBag->controlValues[3], mParameterBag->controlValues[4]), "Alpha");
+
+				// background color
+				static float backcolor[4] = { mParameterBag->controlValues[5], mParameterBag->controlValues[6], mParameterBag->controlValues[7], mParameterBag->controlValues[8] };
+				ImGui::ColorEdit4("back color", backcolor);
+				mParameterBag->controlValues[5] = backcolor[0];
+				mParameterBag->controlValues[6] = backcolor[1];
+				mParameterBag->controlValues[7] = backcolor[2];
+				mParameterBag->controlValues[8] = backcolor[3];
+				ImGui::TextColored(ImVec4(mParameterBag->controlValues[5], 0.0f, 0.0f, 1.0f), "Red"); ImGui::SameLine();
+				ImGui::TextColored(ImVec4(0.0f, mParameterBag->controlValues[6], 0.0f, 1.0f), "Green"); ImGui::SameLine();
+				ImGui::TextColored(ImVec4(0.0f, 0.0f, mParameterBag->controlValues[7], 1.0f), "Blue"); ImGui::SameLine();
+				ImGui::TextColored(ImVec4(mParameterBag->controlValues[5], mParameterBag->controlValues[6], mParameterBag->controlValues[7], mParameterBag->controlValues[8]), "Alpha");
+
+				if (ImGui::Button("Save")) { mParameterBag->save(); }
+
+			}
+			if (ImGui::CollapsingHeader("Log", "2", true, true))
+			{
+				static bool pause;
+				static ImVector<float> values; if (values.empty()) { values.resize(100); memset(&values.front(), 0, values.size()*sizeof(float)); }
+				static int values_offset = 0;
+				if (!pause)
 				{
-					refresh_time = ImGui::GetTime();
-					values[values_offset] = getAverageFps();
-					values_offset = (values_offset + 1) % values.size();
+					static float refresh_time = -1.0f;
+					if (ImGui::GetTime() > refresh_time + 1.0f / 6.0f)
+					{
+						refresh_time = ImGui::GetTime();
+						values[values_offset] = getAverageFps();
+						values_offset = (values_offset + 1) % values.size();
+					}
 				}
+				ImGui::PlotLines("FPS", &values.front(), (int)values.size(), values_offset, toString(floor(getAverageFps())).c_str(), 0.0f, 300.0f, ImVec2(0, 30));
+
+				ImGui::SameLine(); ImGui::Checkbox("pause", &pause);
+				static ImGuiTextBuffer log;
+				static int lines = 0;
+				ImGui::Text("Buffer contents: %d lines, %d bytes", lines, log.size());
+				if (ImGui::Button("Clear")) { log.clear(); lines = 0; }
+				//ImGui::SameLine();
+
+				if (newLogMsg)
+				{
+					newLogMsg = false;
+					log.append(mLogMsg.c_str());
+					lines++;
+					if (lines > 10) { log.clear(); lines = 0; }
+				}
+				ImGui::BeginChild("Log");
+				ImGui::TextUnformatted(log.begin(), log.end());
+				ImGui::EndChild();
 			}
-			ImGui::PlotLines("FPS", &values.front(), (int)values.size(), values_offset, toString(floor(getAverageFps())).c_str(), 0.0f, 300.0f, ImVec2(0, 30));
-
-			ImGui::SameLine(); ImGui::Checkbox("pause", &pause);
-			static ImGuiTextBuffer log;
-			static int lines = 0;
-			ImGui::Text("Buffer contents: %d lines, %d bytes", lines, log.size());
-			if (ImGui::Button("Clear")) { log.clear(); lines = 0; }
-			//ImGui::SameLine();
-
-			if (newLogMsg)
+			if (ImGui::CollapsingHeader("OSC", "2", true, true))
 			{
-				newLogMsg = false;
-				log.append(mLogMsg.c_str());
-				lines++;
-				if (lines > 10) { log.clear(); lines = 0; }
+				static ImGuiTextBuffer OSClog;
+				static int lines = 0;
+				ImGui::Text("Buffer contents: %d lines, %d bytes", lines, OSClog.size());
+				if (ImGui::Button("Clear")) { OSClog.clear(); lines = 0; }
+
+				if (mParameterBag->newOSCMsg)
+				{
+					mParameterBag->newOSCMsg = false;
+					OSClog.append(mParameterBag->OSCMsg.c_str());
+					lines++;
+					if (lines > 10) { OSClog.clear(); lines = 0; }
+				}
+				ImGui::BeginChild("OSClog");
+				ImGui::TextUnformatted(OSClog.begin(), OSClog.end());
+				ImGui::EndChild();
 			}
-			ImGui::BeginChild("Log");
-			ImGui::TextUnformatted(log.begin(), log.end());
-			ImGui::EndChild();
 		}
-		if (ImGui::CollapsingHeader("OSC", "2", true, true))
+		ImGui::ShowTestWindow();
+		ImGui::End();
+		// start a new window
+		ImGui::Begin("Theme Editor", NULL, ImVec2(200, 100));
 		{
-			static ImGuiTextBuffer OSClog;
-			static int lines = 0;
-			ImGui::Text("Buffer contents: %d lines, %d bytes", lines, OSClog.size());
-			if (ImGui::Button("Clear")) { OSClog.clear(); lines = 0; }
+			// our theme variables
+			static ImVec4 color0 = ImVec4(0.16f, 0.16f, 0.16f, 1.0f);
+			static ImVec4 color1 = ImVec4(0.04f, 0.04f, 0.04f, 1.0f);
+			static ImVec4 color2 = ImVec4(0.20f, 0.20f, 0.20f, 1.0f);
+			static ImVec4 color3 = ImVec4(0.69f, 0.69f, 0.69f, 1.0f);
+			static ImVec4 color4 = ImVec4(0.27f, 0.27f, 0.27f, 1.0f);
 
-			if (mParameterBag->newOSCMsg)
-			{
-				mParameterBag->newOSCMsg = false;
-				OSClog.append(mParameterBag->OSCMsg.c_str());
-				lines++;
-				if (lines > 10) { OSClog.clear(); lines = 0; }
+			static float WindowPadding[2] = { 25, 10 };
+			static float WindowMinSize[2] = { 160, 80 };
+			static float FramePadding[2] = { 4, 4 };
+			static float ItemSpacing[2] = { 10, 5 };
+			static float ItemInnerSpacing[2] = { 5, 5 };
+
+			static float WindowFillAlphaDefault = 0.7;
+			static float WindowRounding = 4;
+			static float TreeNodeSpacing = 22;
+			static float ColumnsMinSpacing = 50;
+			static float ScrollBarWidth = 12;
+
+			if (ImGui::CollapsingHeader("Colors")){
+
+				// add radios on one line to switch between color modes
+				static ImGuiColorEditMode colorMode = ImGuiColorEditMode_RGB;
+				ImGui::RadioButton("RGB", &colorMode, ImGuiColorEditMode_RGB);
+				ImGui::SameLine();
+				ImGui::RadioButton("HSV", &colorMode, ImGuiColorEditMode_HSV);
+				ImGui::SameLine();
+				ImGui::RadioButton("HEX", &colorMode, ImGuiColorEditMode_HEX);
+
+				// change color mode
+				ImGui::ColorEditMode(colorMode);
+
+				// add color controls
+				ImGui::ColorEdit4("Color0", (float*)&color0, false);
+				ImGui::ColorEdit4("Color1", (float*)&color1, false);
+				ImGui::ColorEdit4("Color2", (float*)&color2, false);
+				ImGui::ColorEdit4("Color3", (float*)&color3, false);
+				ImGui::ColorEdit4("Color4", (float*)&color4, false);
 			}
-			ImGui::BeginChild("OSClog");
-			ImGui::TextUnformatted(OSClog.begin(), OSClog.end());
-			ImGui::EndChild();
-		}
-	}
-	ImGui::ShowTestWindow();
-	ImGui::End();
-	ImGui::Render();
 
+			if (ImGui::CollapsingHeader("Layout")){
+
+				// layout controls
+				ImGui::SliderFloat2("WindowPadding", WindowPadding, 0, 50, "%.0f");
+				ImGui::SliderFloat2("WindowMinSize", WindowMinSize, 0, 250, "%.0f");
+				ImGui::SliderFloat2("FramePadding", FramePadding, 0, 40, "%.0f");
+				ImGui::SliderFloat2("ItemSpacing", ItemSpacing, 0, 40, "%.0f");
+				ImGui::SliderFloat2("ItemInnerSpacing", ItemInnerSpacing, 0, 40, "%.0f");
+				ImGui::SliderFloat("WindowFillAlphaDefault", &WindowFillAlphaDefault, 0, 1);
+				ImGui::SliderFloat("WindowRounding", &WindowRounding, 0, 15, "%.0f");
+				ImGui::SliderFloat("TreeNodeSpacing", &TreeNodeSpacing, 0, 50, "%.0f");
+				ImGui::SliderFloat("ColumnsMinSpacing", &ColumnsMinSpacing, 0, 150, "%.0f");
+				ImGui::SliderFloat("ScrollBarWidth", &ScrollBarWidth, 0, 35, "%.0f");
+			}
+
+
+			// add a checkbox
+			static bool apply = false;
+			ImGui::Checkbox("Apply", &apply);
+			ImGui::SameLine();
+			if (ImGui::Button("Reset")){
+				color0 = ImVec4(0.16f, 0.16f, 0.16f, 1.0f);
+				color1 = ImVec4(0.04f, 0.04f, 0.04f, 1.0f);
+				color2 = ImVec4(0.20f, 0.20f, 0.20f, 1.0f);
+				color3 = ImVec4(0.69f, 0.69f, 0.69f, 1.0f);
+				color4 = ImVec4(0.27f, 0.27f, 0.27f, 1.0f);
+
+				WindowPadding[0] = WindowPadding[1] = 8;
+				WindowMinSize[0] = WindowMinSize[1] = 48;
+				FramePadding[0] = 5; FramePadding[1] = 4;
+				ItemSpacing[0] = 10; ItemSpacing[1] = 5;
+				ItemInnerSpacing[0] = ItemInnerSpacing[1] = 5;
+
+				WindowFillAlphaDefault = 0.70f;
+				WindowRounding = 4.0f;
+				TreeNodeSpacing = 22.0f;
+				ColumnsMinSpacing = 6.0f;
+				ScrollBarWidth = 16.0f;
+			}
+
+			// show tooltip over checkbox
+			if (ImGui::IsHovered()){
+				ImGui::SetTooltip("Apply the theme colors");
+			}
+
+			// if apply is checked make modifications
+			if (apply){
+				ImGui::setThemeColor(color0, color1, color2, color3, color4);
+
+				ImGui::GetStyle().WindowPadding = ImVec2(WindowPadding[0], WindowPadding[1]);
+				ImGui::GetStyle().WindowMinSize = ImVec2(WindowMinSize[0], WindowMinSize[1]);
+				ImGui::GetStyle().FramePadding = ImVec2(FramePadding[0], FramePadding[1]);
+				ImGui::GetStyle().ItemSpacing = ImVec2(ItemSpacing[0], ItemSpacing[1]);
+				ImGui::GetStyle().ItemInnerSpacing = ImVec2(ItemInnerSpacing[0], ItemInnerSpacing[1]);
+				ImGui::GetStyle().WindowFillAlphaDefault = WindowFillAlphaDefault;
+				ImGui::GetStyle().WindowRounding = WindowRounding;
+				ImGui::GetStyle().TreeNodeSpacing = TreeNodeSpacing;
+				ImGui::GetStyle().ColumnsMinSpacing = ColumnsMinSpacing;
+				ImGui::GetStyle().ScrollBarWidth = ScrollBarWidth;
+			}
+
+			// if log pressed generate code and output to the console
+			if (ImGui::Button("Log to Console")){
+				console() << "ImVec4 color0 = ImVec4( " << color0.x << ", " << color0.y << ", " << color0.z << ", " << color0.w << " );" << endl
+					<< "ImVec4 color1 = ImVec4( " << color1.x << ", " << color1.y << ", " << color1.z << ", " << color1.w << " );" << endl
+					<< "ImVec4 color2 = ImVec4( " << color2.x << ", " << color2.y << ", " << color2.z << ", " << color2.w << " );" << endl
+					<< "ImVec4 color3 = ImVec4( " << color3.x << ", " << color3.y << ", " << color3.z << ", " << color3.w << " );" << endl
+					<< "ImVec4 color4 = ImVec4( " << color4.x << ", " << color4.y << ", " << color4.z << ", " << color4.w << " );" << endl
+					<< "ImGui::setThemeColor( color0, color1, color2, color3, color4 );" << endl << endl
+					<< "ImGuiStyle& style = ImGui::GetStyle();" << endl
+					<< "style.WindowPadding          = ImVec2( " << WindowPadding[0] << "," << WindowPadding[1] << " );" << endl
+					<< "style.WindowMinSize          = ImVec2( " << WindowMinSize[0] << "," << WindowMinSize[1] << " );" << endl
+					<< "style.FramePadding           = ImVec2( " << FramePadding[0] << "," << FramePadding[1] << " );" << endl
+					<< "style.ItemSpacing            = ImVec2( " << ItemSpacing[0] << "," << ItemSpacing[1] << " );" << endl
+					<< "style.ItemInnerSpacing       = ImVec2( " << ItemInnerSpacing[0] << "," << ItemInnerSpacing[1] << " );" << endl
+					<< "style.WindowFillAlphaDefault = " << WindowFillAlphaDefault << ";" << endl
+					<< "style.WindowRounding         = " << WindowRounding << ";" << endl
+					<< "style.TreeNodeSpacing		 = " << TreeNodeSpacing << ";" << endl
+					<< "style.ColumnsMinSpacing		 = " << ColumnsMinSpacing << ";" << endl
+					<< "style.ScrollBarWidth		 = " << ScrollBarWidth << ";" << endl;
+			}
+
+
+			// add a slider to control the background brightness
+			//ImGui::SliderFloat("Background", &f, 0, 1);
+		}
+		ImGui::End();
+		ImGui::Render();
+	}
 	gl::disableAlphaBlending();
 
 }
